da213d2f6d6da123b42942c4ea035bd1d120731e
Aayush Khare <aayushkhare2020@gmail.com>
2025-09-09 17:49:24 -0400

    Add game app with survey link

--- game/app.py
+++ game/app.py
@@ -17,10 +17,34 @@ disease_dict = {
     'Chronic Pain': 'Pain'
 }
 
+technology_dict = {
+    'Deep Brain Stimulation (DBS)': 'DBS',
+    'Focused Ultrasound (FUS)': 'FUS',
+    'Spinal Cord Stimulation (SCS)': 'SCS',
+    'Transcranial Magnetic Stimulation (TMS)': 'TMS',
+    'Vagus Nerve Stimulation (VNS)': 'VNS',
+    'Magnetic Seizure Therapy (MST)': 'MST',
+    'Transcranial Direct Current Stimulation (tDCS)': 'TDCS',
+    'Transcutaneous Electrical Nerve Stimulation (TENS)': 'TENS'
+}
+
 if 'disease' not in st.session_state:
     st.session_state['disease'] = random.choice(disease_options)
 
-select_page = st.sidebar.radio("Contents", ["Introduction", "Brain Disease information", "Choose Neurotechnology"])
+if 'disease_image_index' not in st.session_state:
+    st.session_state['disease_image_index'] = 0  # start with first image
+
+if 'tech_image_index' not in st.session_state:
+    st.session_state['tech_image_index'] = 0  # start with first image
+
+if 'select_tech_list' not in st.session_state:
+    st.session_state['select_tech_list'] = []
+
+select_page = st.sidebar.radio("Contents", 
+                               ["Introduction", 
+                                "Brain Disease information", 
+                                "Choose Neurotechnology", 
+                                "Survey"])
 
 if select_page == "Introduction":
 
@@ -29,12 +53,10 @@ if select_page == "Introduction":
     st.markdown("""
     ### Learning Objectives:""")
     
-    st.markdown("- Learn about brain diseases, their symptoms, demographics, and any neuroscientific basis behing the disease.")
-    st.markdown("- Learn about different neurotechnologies available for the treatment of a disease")
-    st.markdown("- Assess various features of a neurotechnology towards making an informed decision on the best possible neurotechnological treatment of a disease")
-    
-    
-    
+    st.markdown("- Learn about brain diseases, their symptoms, demographics, and any neuroscientific basis behind the disease.")
+    st.markdown("- Learn about different neurotechnologies available for the treatment of a disease.")
+    st.markdown("- Assess various features of a neurotechnology towards making an informed decision on the best possible neurotechnological treatment for a disease.")
+    st.markdown("- Share your thoughts and considerations with others behind your choice of neurotechnology for treatment of a disease")
     st.markdown('''
         <style>
         [data-testid="stMarkdownContainer"] ul{
@@ -42,18 +64,100 @@ if select_page == "Introduction":
         }
         </style>
         ''', unsafe_allow_html=True)
+    
+    st.markdown(""" ### Gameplay Instructions: """)
+    st.markdown("""
+                1. You will be randomly assigned a brain disease from a list of 6 diseases.
+                2. You can view information about the assigned disease including symptoms, demographics, and neuroscientific basis behind the disease.
+                3. You can then choose a neurotechnology that you think is best suited for treatment of the assigned disease from a list of 8 neurotechnologies.
+                4. You can view details about each neurotechnology including features, advantages, and disadvantages.
+                5. You can select one or more neurotechnologies that you think is best suited for treatment of the assigned disease.
+                6. You can then share your thoughts and considerations behind your choice of neurotechnology for treatment of the assigned disease.
+                7. Finally, you can proceed to a survey to share your perspectives on neurotechnologies in a clinical context.
+                """)
 
+    
 elif select_page == "Brain Disease information":
 
     disease_assigned = st.session_state['disease']
 
     st.title(f"You have been assigned the disease: **{disease_assigned}**")
-    st.image(f'images_dis/{disease_dict[disease_assigned]}_1.png', width=1600)
-    st.image(f'images_dis/{disease_dict[disease_assigned]}_2.png', width=1600)
 
+    image_files = [
+        f'images_dis/{disease_dict[disease_assigned]}_1.png',
+        f'images_dis/{disease_dict[disease_assigned]}_2.png'
+    ]
+    st.image(image_files[st.session_state['disease_image_index']], width=500)
+
+    col1, col2 = st.columns([1.6, 1])
+    with col1:
+        if st.button("Front") and st.session_state['disease_image_index'] > 0:
+            st.session_state['disease_image_index'] -= 1
+            st.rerun()
+    with col2:
+        if st.button("Back") and st.session_state['disease_image_index'] < len(image_files) - 1:
+            st.session_state['disease_image_index'] += 1
+            st.rerun()
+    
     if st.button("Choose another disease"):
         st.session_state['disease'] = random.choice(disease_options)
+        st.session_state['disease_image_index'] = 0
         st.rerun()
 
-else:
+elif select_page == "Choose Neurotechnology":
+
     st.title(f"Choose a Neurotechnology that you think is best suited for treatment of {st.session_state['disease']}")
+    
+    col_left, col_right = st.columns([1, 3])
+
+    with col_left:
+        view_technology = st.radio("View Neurotechnology details by selecting from the dropdown",
+                                ['Deep Brain Stimulation (DBS)',
+                                    'Focused Ultrasound (FUS)',
+                                    'Spinal Cord Stimulation (SCS)',
+                                    'Transcranial Magnetic Stimulation (TMS)',
+                                    'Vagus Nerve Stimulation (VNS)',
+                                    'Magnetic Seizure Therapy (MST)',
+                                    'Transcranial Direct Current Stimulation (tDCS)',
+                                    'Transcutaneous Electrical Nerve Stimulation (TENS)'])
+
+    with col_right:
+        if view_technology:
+            image_files = [
+                f'images_tech/{technology_dict[view_technology]}_1.png',
+                f'images_tech/{technology_dict[view_technology]}_2.png'
+            ]
+            st.image(image_files[st.session_state['tech_image_index']], width=1200)
+
+            col1, col2 = st.columns([6, 1])
+            with col1:
+                if st.button("Front") and st.session_state['tech_image_index'] > 0:
+                    st.session_state['tech_image_index'] -= 1
+                    st.rerun()
+            with col2:
+                if st.button("Back") and st.session_state['tech_image_index'] < len(image_files) - 1:
+                    st.session_state['tech_image_index'] += 1
+                    st.rerun()
+
+    st.multiselect(f"Select the Neurotechnology you think is best suited for treatment of {st.session_state['disease']} (you can select multiple)",
+                   ['Deep Brain Stimulation (DBS)',
+                    'Focused Ultrasound (FUS)',
+                    'Spinal Cord Stimulation (SCS)',
+                    'Transcranial Magnetic Stimulation (TMS)',
+                    'Vagus Nerve Stimulation (VNS)',
+                    'Magnetic Seizure Therapy (MST)',
+                    'Transcranial Direct Current Stimulation (tDCS)',
+                    'Transcutaneous Electrical Nerve Stimulation (TENS)'],
+                    key='select_tech_list')
+    
+    if len(st.session_state['select_tech_list']) >= 1:
+
+        st.markdown("### Please note any thoughts behind your choice(s) below for sharing with the other participants:")
+        user_thoughts = st.text_area("Your thoughts", height=200)
+
+    
+else:
+    # link to survey app
+    st.markdown("## Proceed to the Survey to share your perspectives on Neurotechnologies in a clinical context")
+    st.markdown("Click the link below to proceed to the survey:")
+    st.markdown("[Survey Link](https://neurogame-survey.streamlit.app/)")
